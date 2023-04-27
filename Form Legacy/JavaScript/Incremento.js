5c1ca0f8cfb4ed19dc8844641911c68b23d5c5b1
Your Name <you@example.com>
2023-04-27 02:12:55 +0200

    avance

--- Form Legacy/JavaScript/Incremento.js
+++ Form Legacy/JavaScript/Incremento.js
@@ -2,6 +2,7 @@
 
 for (let i = 0; i <  document.querySelectorAll('.boton').length; i++) {
   const boton =  document.querySelectorAll('.boton')[i];
+  
 
   boton.addEventListener('click', function() {
     const precio = parseFloat(boton.dataset.precio);
@@ -9,5 +10,16 @@ for (let i = 0; i <  document.querySelectorAll('.boton').length; i++) {
     console.log(`Total a pagar: ${totalAPagar}`);
     const totalelemento = document.getElementById("total");
     totalelemento.textContent=totalAPagar.toFixed(2)
+    if(botonclickeado==true){
+      boton.children[1].style.transform = "rotate(180deg)";
+      console.log("clickeado");
+      botonclickeado=false;
+    }else{
+      boton.children[1].style.transform = "rotate(-180deg)";
+      console.log("no clickeado");
+      botonclickeado=true;
+    }
   });
+ 
 }
+
