95320bcabb93883dd968fb5db70dd1f391935309
Your Name <you@example.com>
2023-04-29 03:41:10 +0200

    Terminado

--- Form Legacy/JavaScript/Incremento.js
+++ Form Legacy/JavaScript/Incremento.js
@@ -11,16 +11,7 @@ for (let i = 0; i <  document.getElementsByClassName('boton').length; i++) {
     console.log(`Total a pagar: ${totalAPagar}`);
     const totalelemento = document.getElementById("total");
     totalelemento.textContent=`Su total es: ${totalAPagar.toFixed(2)}`
-    if(botonclickeado==true){
-      boton.children[1].style.transform = "rotate(180deg)";
-      console.log("clickeado");
-      botonclickeado=false;
-
-    }else{
-      boton.children[1].style.transform = "rotate(-180deg)";
-      console.log("no clickeado");
-      botonclickeado=true;
-    }
+    
     
     
       
--- Form Legacy/JavaScript/VerFoto.js
+++ Form Legacy/JavaScript/VerFoto.js
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
+
 const elemento = document.getElementsByClassName(`foto`);
 const cantidad = elemento.length;
-=======
 
 
-for(i=0;i< document.getElementsByClassName(`foto`).length;i++){
 
-const elemento = document.getElementsByClassName(`foto`)[i];
-const height = elemento.clientHeight;
-const width = elemento.clientWidth;
->>>>>>> 7b681049f23c9b67879a4a1cec600ec98b5956c5
 
 for(i=0;i<cantidad;i++){
 
@@ -25,14 +19,11 @@ cadafoto.addEventListener("mousemove", (evt) => {
   cadafoto.style.transform = transformString;
 });
 
-<<<<<<< HEAD
+
 cadafoto.addEventListener("mouseout", () => {
   cadafoto.style.transform = "perspective(500px) scale(1) rotateX(0) rotateY(0)";
 });
 }
-=======
-elemento.addEventListener("mouseout", () => {
-  elemento.style.transform = "perspective(500px) scale(1) rotateX(0) rotateY(0)";
-});
-}
->>>>>>> 7b681049f23c9b67879a4a1cec600ec98b5956c5
+
+
+
