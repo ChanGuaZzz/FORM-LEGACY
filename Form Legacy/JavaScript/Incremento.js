28aa7ca1c2e5bd26c365bce10cd4fb56dae7016e
Your Name <you@example.com>
2023-04-29 13:33:51 +0200

    Terminado

--- Form Legacy/JavaScript/Incremento.js
+++ Form Legacy/JavaScript/Incremento.js
@@ -2,6 +2,7 @@
 
 for (let i = 0; i <  document.getElementsByClassName('boton').length; i++) {
   const boton =  document.getElementsByClassName('boton')[i];
+  const totalelemento = document.getElementById("total");
   
 
   boton.addEventListener('click', function() {
@@ -9,7 +10,7 @@ for (let i = 0; i <  document.getElementsByClassName('boton').length; i++) {
     const precio = parseFloat(boton.dataset.precio);
     totalAPagar += precio;
     console.log(`Total a pagar: ${totalAPagar}`);
-    const totalelemento = document.getElementById("total");
+    
     totalelemento.textContent=`Su total es: ${totalAPagar.toFixed(2)}`
     
     
